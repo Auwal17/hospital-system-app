65f4ec3eabccf78174e52c64b165e1c95b3fbea1
Auwal Sani Abdullahi <auwalsaneeabdullahi@gmail.com>
2026-05-18 06:33:06 +0100

    Added production DB settings and requirements

--- backend/hms_core/settings.py
+++ backend/hms_core/settings.py
@@ -9,7 +9,11 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+from typing import Any
 
+
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,8 +29,7 @@ SECRET_KEY = 'django-insecure-ksazr06e$an2ef!)gqth&_691(@&++s^r(p$3^hi^jsi)#@g9$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -81,12 +84,13 @@ WSGI_APPLICATION = 'hms_core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+# Check if we are running in the cloud (Render will provide a DATABASE_URL)
+# If not, fall back to our local SQLite file so you can still test on your computer!
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
